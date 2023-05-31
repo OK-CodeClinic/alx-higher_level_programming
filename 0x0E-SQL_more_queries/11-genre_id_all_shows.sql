24be7cf5de816aed40c3789e1a480ecb5c4869f4
OK-CodeClinic <kehindefranklin@gmail.com>
2023-05-31 12:00:50 -0700

    Listing all show in the database

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,5 @@
--- This lists all shows contained in the database hbtn_0d_tvshows
--- This lists all rows of tables in a database
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+-- Script that lists all cities contained in the database
+SELECT tv_shows.title, tv_show_genres.genre_id -- Query to join cities and states
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
